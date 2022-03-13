1bdd3a503f7a26258679a0343a192e32c74438d8
Patrick Organ <patrick@permafrost.dev>
2022-03-13 12:22:49 -0400

    wip

--- src/Middleware/SendErrorToRay.ts
+++ src/Middleware/SendErrorToRay.ts
@@ -3,20 +3,22 @@ import { ray } from 'node-ray';
 export const sendErrorToRay = (err, req, res, next) => {
     const { method, url, params, query } = req;
 
-    ray().table({
-        method,
-        url,
-        params,
-        query,
-    });
+    ray().table(
+        {
+            error: err.message,
+            method,
+            url,
+            params,
+            query,
+        },
+        'Error',
+    );
 
     if (res.headersSent) {
         return next(err);
     }
 
-    res.status(500);
-
-    res.send({ error: err });
+    next(err);
 };
 
 export default sendErrorToRay;
