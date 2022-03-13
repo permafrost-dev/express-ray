cf8550bdb307b1493a615846e105704143a8a0b1
Patrick Organ <patrick@permafrost.dev>
2022-03-13 07:14:45 -0400

    wip

--- scripts/build.js
+++ scripts/build.js
@@ -5,19 +5,34 @@ const esbuild = require('esbuild');
 const { basename } = require('path');
 const { spawnSync } = require('child_process');
 
-const buildConfig = {
-    basePath: `${__dirname}/..`,
-    outdir: 'dist',
-    format: 'cjs',
-    entry: 'src/index.ts',
-    bundle: true,
-    minify: false,
-    constants: {},
-    platform: {
-        name: 'node',
-        version: 16,
+const buildConfigs = [
+    {
+        basePath: `${__dirname}/..`,
+        outfile: 'dist/index.cjs.js',
+        format: 'cjs',
+        entry: 'src/index.ts',
+        bundle: true,
+        minify: false,
+        constants: {},
+        platform: {
+            name: 'node',
+            version: 16,
+        },
     },
-};
+    {
+        basePath: `${__dirname}/..`,
+        outfile: 'dist/index.esm.mjs',
+        format: 'esm',
+        entry: 'src/index.ts',
+        bundle: true,
+        minify: false,
+        constants: {},
+        platform: {
+            name: 'node',
+            version: 16,
+        },
+    },
+];
 
 class Builder {
     config = {
@@ -34,26 +49,31 @@ class Builder {
     }
 
     async compile() {
-        const result = await esbuild.build({
-            logLevel: 'silent',
-            absWorkingDir: buildConfig.basePath,
-            entryPoints: [buildConfig.entry],
-            outdir: buildConfig.outdir,
-            bundle: buildConfig.bundle,
-            format: buildConfig.format,
-            platform: buildConfig.platform.name,
-            target: `${buildConfig.platform.name}${buildConfig.platform.version}`,
-            allowOverwrite: true,
-            minify: buildConfig.minify,
-            metafile: true,
-            define: {
-                __APP_VERSION__: `'${require(realpathSync(`${buildConfig.basePath}/package.json`, { encoding: 'utf-8' })).version}'`,
-                __COMPILED_AT__: `'${new Date().toUTCString()}'`,
-                ...buildConfig.constants,
-            },
+        const results = [];
+
+        buildConfigs.forEach(async buildConfig => {
+            const result = await esbuild.build({
+                logLevel: 'silent',
+                absWorkingDir: buildConfig.basePath,
+                entryPoints: [buildConfig.entry],
+                outfile: buildConfig.outfile,
+                bundle: buildConfig.bundle,
+                format: buildConfig.format,
+                platform: buildConfig.platform.name,
+                target: `${buildConfig.platform.name}${buildConfig.platform.version}`,
+                allowOverwrite: true,
+                minify: buildConfig.minify,
+                metafile: true,
+                define: {
+                    __APP_VERSION__: `'${require(realpathSync(`${buildConfig.basePath}/package.json`, { encoding: 'utf-8' })).version}'`,
+                    __COMPILED_AT__: `'${new Date().toUTCString()}'`,
+                    ...buildConfig.constants,
+                },
+            });
+            results.push(result);
         });
 
-        return new Promise(resolve => resolve(result));
+        return new Promise(resolve => resolve(results));
     }
 
     sizeForDisplay(bytes) {
@@ -88,17 +108,17 @@ class Builder {
             .forEach(data => (this.config[data.name] = data.value));
     }
 
-    convertToProductionFile() {
-        const filename = basename(buildConfig.entry).replace(/\.ts$/, '.js');
-        const newFilename = require(realpathSync(`${buildConfig.basePath}/package.json`, { encoding: 'utf-8' })).name;
+    // convertToProductionFile() {
+    //     const filename = basename(buildConfig.entry).replace(/\.ts$/, '.js');
+    //     const newFilename = require(realpathSync(`${buildConfig.basePath}/package.json`, { encoding: 'utf-8' })).name;
 
-        spawnSync('chmod', ['+x', `${buildConfig.outdir}/${filename}`], { stdio: 'ignore' });
+    //     spawnSync('chmod', ['+x', `${buildConfig.outdir}/${filename}`], { stdio: 'ignore' });
 
-        const contents = readFileSync(`${buildConfig.outdir}/${filename}`, { encoding: 'utf-8' });
+    //     const contents = readFileSync(`${buildConfig.outdir}/${filename}`, { encoding: 'utf-8' });
 
-        writeFileSync(`${buildConfig.outdir}/${filename}`, `#!/usr/bin/node\n\n${contents}`, { encoding: 'utf-8' });
-        renameSync(`${buildConfig.outdir}/${filename}`, `${buildConfig.outdir}/${newFilename}`);
-    }
+    //     writeFileSync(`${buildConfig.outdir}/${filename}`, `#!/usr/bin/node\n\n${contents}`, { encoding: 'utf-8' });
+    //     renameSync(`${buildConfig.outdir}/${filename}`, `${buildConfig.outdir}/${newFilename}`);
+    // }
 
     async run() {
         this.processArgv();
@@ -114,14 +134,14 @@ class Builder {
         const finishedTs = new Date().getTime();
 
         if (this.config.verbose) {
-            this.reportCompileResults(results);
+            results.forEach(result => this.reportCompileResults(result));
         }
 
         this.writeln((this.config.verbose ? `* D` : `d`) + `one. (${finishedTs - startedTs} ms)`);
 
-        if (this.config.production) {
-            this.convertToProductionFile();
-        }
+        // if (this.config.production) {
+        //     this.convertToProductionFile();
+        // }
     }
 }
 
