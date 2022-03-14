3c6a86278d91c1aeb49bfc1a5f5ff511983c201f
Patrick Organ <patrick@permafrost.dev>
2022-03-14 14:21:14 -0400

    make SendRequestToRay middleware configurable, add micromatch package for path matching

--- src/Middleware/SendRequestToRay.ts
+++ src/Middleware/SendRequestToRay.ts
@@ -1,25 +1,44 @@
 import { ray } from 'node-ray';
+import micromatch from 'micromatch';
+export interface SendRequestToRayOptions {
+    matchPaths?: string[];
+    ignorePaths?: string[];
+}
 
-export const sendRequestToRay = (req, res, next, rayInstance: any = null) => {
-    const { method, url, params, query, headers } = req;
+export const sendRequestToRay = (options: SendRequestToRayOptions = {}) => {
+    const matchPaths = options.matchPaths || ['*'];
+    const ignorePaths = options.ignorePaths || [];
 
-    rayInstance = rayInstance ?? ray();
+    const result = (req, res, next, rayInstance: any = null) => {
+        const matchedPath = micromatch.isMatch(req.path, matchPaths);
+        const isIgnoredPath = micromatch.isMatch(req.path, ignorePaths);
 
-    const data = {
-        method,
-        url,
-        params,
-        query,
-        headers,
+        if (!matchedPath || isIgnoredPath) {
+            next();
+        }
+
+        const { method, url, params, query, headers } = req;
+
+        rayInstance = rayInstance ?? ray();
+
+        const data = {
+            method,
+            url,
+            params,
+            query,
+            headers,
+        };
+
+        if (method.toLowerCase() === 'post') {
+            data['body'] = req.body;
+        }
+
+        rayInstance.table(data, 'Request');
+
+        next();
     };
 
-    if (method.toLowerCase() === 'post') {
-        data['body'] = req.body;
-    }
-
-    rayInstance.table(data, 'Request');
-
-    next();
+    return result;
 };
 
 export default sendRequestToRay;
