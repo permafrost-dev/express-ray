60bee61ada769013d0b356105bd6999fbd7b8862
Patrick Organ <patrick@permafrost.dev>
2022-03-14 19:26:54 -0400

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
         '^.+\\.tsx?$': 'ts-jest',
     },
     testRegex: '(/__test__/.*|/tests/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/tests/TestClasses/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
     moduleNameMapper: pathsToModuleNameMapper(tsConfigPaths, { prefix: `${__dirname}/` }),
 
